f1ec81c0537944954673607d0d88789df492407c
Daniel Alencar <danielalencaralencar0@gmail.com>
2023-12-08 16:14:12 -0300

    Calculadora basica

---
+++ CalcType1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CalcType1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btSomar = new System.Windows.Forms.Button();
+            this.btMultiplicar = new System.Windows.Forms.Button();
+            this.btSubtrair = new System.Windows.Forms.Button();
+            this.btDividir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(4, 29);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(75, 20);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(85, 29);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(81, 20);
+            this.txtNumero2.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(172, 29);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(69, 20);
+            this.txtResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Número 01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Número 02";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resultado";
+            // 
+            // btSomar
+            // 
+            this.btSomar.Location = new System.Drawing.Point(4, 68);
+            this.btSomar.Name = "btSomar";
+            this.btSomar.Size = new System.Drawing.Size(113, 23);
+            this.btSomar.TabIndex = 6;
+            this.btSomar.Text = "Somar";
+            this.btSomar.UseVisualStyleBackColor = true;
+            this.btSomar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btMultiplicar
+            // 
+            this.btMultiplicar.Location = new System.Drawing.Point(123, 68);
+            this.btMultiplicar.Name = "btMultiplicar";
+            this.btMultiplicar.Size = new System.Drawing.Size(118, 23);
+            this.btMultiplicar.TabIndex = 7;
+            this.btMultiplicar.Text = "Multiplicar";
+            this.btMultiplicar.UseVisualStyleBackColor = true;
+            this.btMultiplicar.Click += new System.EventHandler(this.btMultiplicar_Click);
+            // 
+            // btSubtrair
+            // 
+            this.btSubtrair.Location = new System.Drawing.Point(4, 97);
+            this.btSubtrair.Name = "btSubtrair";
+            this.btSubtrair.Size = new System.Drawing.Size(113, 23);
+            this.btSubtrair.TabIndex = 9;
+            this.btSubtrair.Text = "Subtrair";
+            this.btSubtrair.UseVisualStyleBackColor = true;
+            this.btSubtrair.Click += new System.EventHandler(this.btSubtrair_Click);
+            // 
+            // btDividir
+            // 
+            this.btDividir.Location = new System.Drawing.Point(123, 97);
+            this.btDividir.Name = "btDividir";
+            this.btDividir.Size = new System.Drawing.Size(118, 23);
+            this.btDividir.TabIndex = 10;
+            this.btDividir.Text = "Dividir";
+            this.btDividir.UseVisualStyleBackColor = true;
+            this.btDividir.Click += new System.EventHandler(this.btDividir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(245, 156);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btDividir);
+            this.Controls.Add(this.btSubtrair);
+            this.Controls.Add(this.btMultiplicar);
+            this.Controls.Add(this.btSomar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CalcType";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btSomar;
+        private System.Windows.Forms.Button btMultiplicar;
+        private System.Windows.Forms.Button btSubtrair;
+        private System.Windows.Forms.Button btDividir;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CalcType1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcType1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Soma
+        {
+            Processos p = new Processos();
+
+            //Configura os valores de numero1 e numero2 com base nos textBox
+            p.setNumero1(txtNumero1.Text);
+            p.setNumero2(txtNumero2.Text);
+
+            // Obtém o valor da propriedade txtNumero1 como uma string
+            string numero01String = p.getNumero1();
+            string numero02String = p.getNumero2();
+
+            // Tenta converter a string para double
+            if (double.TryParse(numero01String, out double numero01) && double.TryParse(numero02String, out double numero02))
+            {
+                // Somando os valores
+                double resultado = numero01 + numero02;
+
+                //Define o resultado na propriedade Result do processos(set/get)
+                p.setResult(resultado.ToString());
+
+                //Configura o resultado no TexBox txtresult
+                txtResult.Text = resultado.ToString();
+                
+            }
+            else
+            {
+                // Lidar com o caso em que a conversão falha
+                Console.WriteLine("Falha na conversão para double.");
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) //Limpar os campos
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            txtResult.Text = "";
+        }
+
+        private void btSubtrair_Click(object sender, EventArgs e)
+        {
+            Processos p = new Processos();
+
+            p.setNumero1(txtNumero1.Text);
+            p.setNumero2(txtNumero2.Text);
+
+            //Processo da linha 28 e 32 em uma unica linha
+            double numero1 = double.TryParse(p.getNumero1(), out double parsedValue) ? parsedValue : 0.0;
+            double numero2 = double.TryParse(p.getNumero2(), out double parsedValue1) ? parsedValue1 : 0.0;
+
+            //Subtração
+            double resultado = numero1 - numero2;
+
+            p.setResult(resultado.ToString());
+            txtResult.Text = resultado.ToString();
+        }
+
+        private void btMultiplicar_Click(object sender, EventArgs e)
+        {
+            Processos p = new Processos();
+
+            p.setNumero1(txtNumero1.Text);
+            p.setNumero2(txtNumero2.Text);
+
+            double numero1 = double.TryParse(p.getNumero1(), out double parsedValue) ? parsedValue : 0.0;
+            double numero2 = double.TryParse(p.getNumero2(), out double parsedValue1) ? parsedValue1 : 0.0;
+
+            double resultado = numero1 * numero2;
+
+            p.setResult(resultado.ToString());
+            txtResult.Text = resultado.ToString();
+        }
+
+        private void btDividir_Click(object sender, EventArgs e)
+        {
+            Processos p = new Processos();
+
+            p.setNumero1(txtNumero1.Text);
+            p.setNumero2(txtNumero2.Text);
+
+            double numero1 = double.TryParse(p.getNumero1(), out double parsedValue) ? parsedValue : 0.0;
+            double numero2 = double.TryParse(p.getNumero2(), out double parsedValue1) ? parsedValue1 : 0.0;
+
+            double resultado = numero1 / numero2;
+
+            p.setResult(resultado.ToString());
+            txtResult.Text = resultado.ToString();
+        }
+
+        private void btPorcentagem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
